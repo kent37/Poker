a5aea23a86fc691e90375a1cc149bfad3811cee1
Kent Johnson <kent3737@gmail.com>
2018-06-21 21:04:00 -0400

    More complete ranking of hands. Now includes high card(s) and kicker(s)

--- R/Hands.R
+++ R/Hands.R
@@ -30,23 +30,128 @@ rank_hand = function(hand) {
   suits = purrr::map_chr(hand, 'suit')
   max_flush = max(table(suits))
 
-  # Now we can make a first cut at finding the category
-  category = dplyr::case_when(
-    max_flush==5 && stringr::str_detect(diffs, '1111') ~ 'Straight Flush',
-    stringr::str_detect(diffs, '000') ~ 'Four of a Kind',
-    stringr::str_detect(diffs, '00.0')
-      || stringr::str_detect(diffs, '0.00') ~ 'Full House',
-    max_flush==5 ~ 'Flush',
-    stringr::str_detect(diffs, '1111') ~ 'Straight',
-    stringr::str_detect(diffs, '00') ~ 'Three of a Kind',
-    stringr::str_detect(diffs, '0.+0') ~ 'Two Pair',
-    stringr::str_detect(diffs, '0') ~ 'One Pair',
-    TRUE ~ 'High Card'
-  )
-
-  # Look for a royal straight flush
-  if (category == 'Straight Flush' && ranks[1] == 14)
-    category = 'Royal Straight Flush'
-
-  category
+  # Now try all the testers and find the one that sticks
+  testers = c(is_straight_flush, is_four_of_a_kind, is_full_house,
+              is_flush, is_straight, is_three_of_a_kind, is_two_pair,
+              is_pair, is_high_card)
+  for (tester in testers) {
+    category = tester(hand, diffs, max_flush)
+    if (!is.null(category))
+      return (category)
+  }
 }
+
+# These functions all take a hand, diffs and max_flush as input and return either a
+# category object (a list with a name, high card(s) and kickers)
+# or NULL
+is_straight_flush = function(hand, diffs, max_flush) {
+  if (max_flush!=5 || !stringr::str_detect(diffs, '1111'))
+    return (NULL)
+
+  # It's some kind of straight flush
+  if (hand[[1]]$rank == 14)
+    return (as.category('Royal Straight Flush', NULL, NULL))
+
+  return (as.category('Straight Flush', hand[1], NULL))
+}
+
+is_four_of_a_kind = function(hand, diffs, max_flush) {
+  m = stringr::str_locate(diffs, '000')
+  loc = unname(m[1, 1])
+  if (is.na(loc)) return (NULL)
+  high = hand[loc]
+  kicker = ifelse(loc==1, hand[5], hand[1])
+  return (as.category('Four of a Kind', high, kicker))
+}
+
+is_full_house = function(hand, diffs, max_flush) {
+  if (stringr::str_detect(diffs, '00.0')) # Three then two
+    return (as.category('Full House', hand[c(1, 4)], NULL))
+
+  if (stringr::str_detect(diffs, '0.00')) # Two then three
+    return (as.category('Full House', hand[c(3, 1)], NULL))
+
+  return (NULL)
+}
+
+is_straight = function(hand, diffs, max_flush) {
+  if (stringr::str_detect(diffs, '1111'))
+    return (as.category('Straight', hand[1], NULL))
+
+  return (NULL)
+}
+
+is_flush = function(hand, diffs, max_flush) {
+  if (max_flush == 5)
+    return (as.category('Flush', hand[1], NULL))
+
+  return (NULL)
+}
+
+
+is_three_of_a_kind = function(hand, diffs, max_flush) {
+  m = stringr::str_locate(diffs, '00')
+  loc = m[1, 1]
+  if (is.na(loc)) return (NULL)
+
+  kickers = setdiff(1:5, loc:(loc+2))
+  return (as.category('Three of a Kind', hand[loc], hand[kickers]))
+}
+
+is_two_pair = function(hand, diffs, max_flush) {
+  m = stringr::str_locate(diffs, '0.+0')
+  p1 = m[1, 1]
+  if (is.na(p1)) return (NULL)
+  p2 = m[1, 2]
+  kicker = setdiff(1:5, c(p1, p1+1, p2, p2+1))
+  return (as.category('Two Pair', hand[c(p1, p2)], hand[kicker]))
+}
+
+is_pair = function(hand, diffs, max_flush) {
+  m = stringr::str_locate(diffs, '0')
+  loc = m[1, 1]
+  if (is.na(loc)) return (NULL)
+
+  kickers = setdiff(1:5, c(loc, loc+1))
+  return (as.category('One Pair', hand[loc], hand[kickers]))
+}
+
+is_high_card = function(hand, diffs, max_flush) {
+  return (as.category('High Card', hand[1], hand[2:5]))
+}
+
+as.category = function(name, high, kickers) {
+  structure(list(name=name, high=high, kickers=kickers),
+            class=c('category', 'list'))
+}
+
+#' Construct the string representation of a category
+#' @param x A category object
+#' @param ... Ignored
+#' @return The string representation of the category
+#' @export
+format.category = function(x, ...) {
+  name = x$name
+  high = purrr::map_int(x$high, 'rank')
+  high = card_names[high-1]
+  kickers = purrr::map_int(x$kickers, 'rank')
+  kickers = card_names[kickers-1]
+
+  high_str = dplyr::case_when(
+    length(high)==0 ~ NA_character_,
+    length(high)==1 ~ paste(high, 'High'),
+    (length(high)==2 && name=='Full House') ~ paste(high[1], 'over', high[2]),
+    (length(high)==2 && name=='Two Pair') ~ paste(high[1], 'and', high[2])
+  )[1]
+
+  kicker_str = ifelse(length(kickers)==0, NA_character_,
+                      paste('Kickers:', paste(kickers, collapse=', ')))
+
+  paste(na.omit(c(name, high_str, kicker_str)), collapse=', ')
+}
+
+#' Print a category
+#' @param x A category object
+#' @param ... Passed to `print.default`
+#' @export
+print.category = function(x, ...) print(format(x), ...)
--- tests/testthat/test-hands.R
+++ tests/testthat/test-hands.R
@@ -4,21 +4,26 @@ context("test-hands.R")
 # One hand of each type, and its type
 test_hands = list(
   c('["AS", "10S", "QS", "JS", "KS"]', 'Royal Straight Flush'),
-  c('["9S", "10S", "QS", "JS", "KS"]', 'Straight Flush'),
-  c('["3H", "3S", "3D", "4H", "3C"]', 'Four of a Kind'),
-  c('["JS", "3D", "JH", "3C", "JC"]', 'Full House'), # Three of a kind high
-  c('["3S", "3D", "JH", "3C", "JC"]', 'Full House'), # Pair high
-  c('["3H", "10H", "2H", "AH", "7H"]', 'Flush'),
-  c('["8S", "QH", "JS", "9D", "10C"]', 'Straight'),
-  c('["3H", "3S", "5D", "4H", "3C"]', 'Three of a Kind'),
-  c('["3H", "3S", "KD", "4H", "KC"]', 'Two Pair'),
-  c('["AS", "KD", "AD", "3C", "9H"]', 'One Pair'),
-  c('["1H", "2H", "3C", "4H", "6H"]', 'High Card')) # Heart breaker...
+  c('["9S", "10S", "QS", "JS", "KS"]', 'Straight Flush, K High'),
+  c('["3H", "3S", "3D", "4H", "3C"]', 'Four of a Kind, 3 High, Kickers: 4'),
+  c('["JS", "3D", "JH", "3C", "JC"]', 'Full House, J over 3'), # Three of a kind high
+  c('["3S", "3D", "JH", "3C", "JC"]', 'Full House, 3 over J'), # Pair high
+  c('["3H", "10H", "2H", "AH", "7H"]', 'Flush, A High'),
+  c('["8S", "QH", "JS", "9D", "10C"]', 'Straight, Q High'),
+  c('["3H", "3S", "5D", "4H", "3C"]', 'Three of a Kind, 3 High, Kickers: 5, 4'),
+  c('["3H", "3S", "KD", "4H", "KC"]', 'Two Pair, K and 3, Kickers: 4'),
+  c('["AS", "KD", "AD", "3C", "9H"]', 'One Pair, A High, Kickers: K, 9, 3'),
+  c('["7H", "2H", "3C", "4H", "6H"]', 'High Card, 7 High, Kickers: 6, 4, 3, 2')) # Heart breaker...
 
 test_that("rank_hand works", {
   for (case in test_hands) {
     hand = parse_cards(case[1])
     expected_rank = case[2]
-    expect_equal(rank_hand(hand), expected_rank, info=expected_rank)
+    expect_equal(format(rank_hand(hand)), expected_rank, info=expected_rank)
   }
 })
+
+# Make every possible hand and see if it can be ranked
+# test_that('All hands can be ranked', {
+#
+# })
